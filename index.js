72e6fc91ae4dfaeaacb3cb1475b8279a17fa5841
Sri Lakshmi Narasimha Settipalli <narasimhas9490@gmail.com>
2024-05-19 18:50:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 window.addEventListener('load' , ()=>{
-      if (Notification.permission === 'denied') {
-    alert('Notification permission is denied. Click on "i" side to home symbol at top and reset permissions.');
-}
+    
     if( Notification.permission == 'granted'){
         document.getElementById("notgranted").style.display = "none" ;
         document.getElementById("granted").style.display = "flex" ;
     }
     document.getElementById("notify").addEventListener("click" , function(){
+            if (Notification.permission === 'denied') {
+    alert('Notification permission is denied. Click on "i" side to home symbol at top and reset permissions.');
+}
         Notification.requestPermission().then(()=>{
       if( Notification.permission == 'granted'  ){
         document.getElementById("notgranted").style.display = "none" ;
